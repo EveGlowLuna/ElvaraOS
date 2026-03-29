fcc93e928bcbae607f92e1aeacf2d2c98387471d
eveglowluna <yy15502497613@outlook.com>
2026-03-29 13:53:31 +0800

    fix:构建脚本

--- build-workflow.sh
+++ build-workflow.sh
@@ -3,13 +3,13 @@
 set -e
 
 # 安装 archiso 及依赖
-sudo pacman -Sy --needed --noconfirm archiso || { echo "安装 archiso 失败"; exit 1; }
+pacman -Sy --needed --noconfirm archiso || { echo "安装 archiso 失败"; exit 1; }
 
 # 创建工作目录
 mkdir -p /tmp/work
 
 # 构建 ISO
-sudo mkarchiso -v -w /tmp/work -o "./output" . || { echo "mkarchiso 构建失败"; exit 1; }
+mkarchiso -v -w /tmp/work -o "./output" . || { echo "mkarchiso 构建失败"; exit 1; }
 
 # 赋予 ISO 可执行权限
 chmod +x ./output/*.iso || { echo "chmod 失败"; exit 1; }
