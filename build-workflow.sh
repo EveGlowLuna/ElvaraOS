93ff8f8a46582c3edbbb01d1a71421bf8bf81765
eveglowluna <yy15502497613@outlook.com>
2026-03-29 13:50:04 +0800

    ci:自动化

---
+++ build-workflow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# 安装 archiso 及依赖
+sudo pacman -Sy --needed --noconfirm archiso || { echo "安装 archiso 失败"; exit 1; }
+
+# 创建工作目录
+mkdir -p /tmp/work
+
+# 构建 ISO
+sudo mkarchiso -v -w /tmp/work -o "./output" . || { echo "mkarchiso 构建失败"; exit 1; }
+
+# 赋予 ISO 可执行权限
+chmod +x ./output/*.iso || { echo "chmod 失败"; exit 1; }
+
+echo "构建成功"
+exit 0
