9f036cb2354541873f4b9e7985541d6554f5e591
EveGlow <yy15502497613@outlook.com>
2026-04-04 23:52:50 +0800

    sync：Installer

--- airootfs/usr/local/share/ElvaraInstaller/custom/custom.py
+++ airootfs/usr/local/share/ElvaraInstaller/custom/custom.py
@@ -2,6 +2,6 @@ import shutil
 from installer import base_system
 
 def run(mount_point):
-    print("请修改文件来实现自定义效果。这是一个自定义效果，执行customize_system.sh")
+    # 请修改文件来实现自定义效果。这是一个示例自定义效果（也是ElvaraOS最终效果），执行customize_system.sh
     shutil.copy("customize_system.sh", f'{mount_point}/root/customize_system.sh')
     base_system.arch_chroot(mount_point, ['bash', '/root/customize_system.sh'])
